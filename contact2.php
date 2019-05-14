9830d4fae4c20b237624ad7ec260ce6064891989
Meaghan Bass <45584434+meaghanbass@users.noreply.github.com>
2019-05-14 13:00:33 -0700

    now includes contact2 page with mailchimp form to add inquiries to mailchimp account. both contact and contact2 are in folder, may choose later

---
+++ contact2.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139871568-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-139871568-1');
+    </script>
+  <!-- End Global site tag (gtag.js) - Google Analytics -->
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#000000">
+
+  <link rel="manifest" href="./manifest.webmanifest">
+  <link rel="shortcut icon" href="images/favicon.png">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css">
+  <link rel="stylesheet" href="./src/index.css">
+  <link rel="stylesheet" href="./src/app.css">
+
+  <link rel="stylesheet" href="css/rootcolors.css" type="text/css">
+  <link rel="stylesheet" href="css/styles.css" type="text/css">
+  <link rel="stylesheet" href="css/bootstrap-adjacent.css" type="text/css">
+  <link rel="stylesheet" href="css/ais-instantsearch-adjacent.css" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/algolia-min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  
+  <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <title>Contact - Global Web Properties</title>
+</head>
+
+<body>
+  
+<!-- NAVBAR -->
+<?php include 'php/navbar.php';?>
+<!--END NAVBAR -->
+
+<!-- BEGIN HEADER -->
+<div class="jumbotron text-center tag contact-jumbotron-gradient" style="margin-bottom:0;">
+  <h1 class="font-poppins">
+    <span class="solstice">GET IN TOUCH</span>
+  </h1>
+  <p class="font-hind nova tag_sub">Use the form below to get in touch with us.</p> 
+</div>
+<!-- END HEADER -->
+
+<!-- BEGIN CONTACT FORM CONTAINER -->
+<div class="container-fluid contact-form-container">
+    <!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup" class="col-sm col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 font-poppins" style="background-color:transparent;">
+    <form action="https://gmail.us20.list-manage.com/subscribe/post?u=6324e563bdd15c95ab519a5bc&amp;id=973c822e15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        
+    <div class="mc-field-group form-label">
+        <label for="mce-INQUIRY">Nature of Inquiry: </label>
+        <select name="INQUIRY" class="" id="mce-INQUIRY">
+            <option value=""></option>
+            <option value="Web Design">Web Design</option>
+            <option value="Hosting">Hosting</option>
+            <option value="Site Management">Site Management</option>
+            <option value="App Development">App Development</option>
+            <option value="Solutions Architecture">Solutions Architecture</option>
+        </select>
+    </div>
+
+    <div class="mc-field-group form-label">
+        <label for="mce-COMPANY">Company </label>
+        <input type="text" value="" name="COMPANY" class="" id="mce-COMPANY">
+    </div>
+
+    <div class="mc-field-group form-label">
+        <label for="mce-FNAME">First Name </label>
+        <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+    </div>
+
+    <div class="mc-field-group form-label">
+        <label for="mce-LNAME">Last Name </label>
+        <input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+    </div>
+
+    <div class="mc-field-group size1of2 form-label">
+        <label for="mce-PHONE">Phone Number </label>
+        <input type="text" name="PHONE" class="" value="" id="mce-PHONE">
+    </div>
+
+    <div class="mc-field-group form-label">
+        <label for="mce-EMAIL">Email </label>
+        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+    </div>
+
+    <div class="mc-field-group form-label">
+        <label for="mce-MESSAGE">Message </label>
+        <textarea type="text" rows="5" value="" name="MESSAGE" class="contact-form-textarea" id="mce-MESSAGE"></textarea>
+    </div>
+
+        <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6324e563bdd15c95ab519a5bc_973c822e15" tabindex="-1" value=""></div>
+        
+        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        
+        </div>
+    </form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[5]='INQUIRY';ftypes[5]='dropdown';fnames[2]='COMPANY';ftypes[2]='text';fnames[1]='FNAME';ftypes[1]='text';fnames[3]='LNAME';ftypes[3]='text';fnames[4]='PHONE';ftypes[4]='phone';fnames[0]='EMAIL';ftypes[0]='email';fnames[6]='MESSAGE';ftypes[6]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
+</div>
+
+<!-- END CONTACT FORM CONTAINER -->
+
+<!-- BEGIN FOOTER -->
+<?php include 'php/footer.php';?>
+<!-- END FOOTER -->
+
+</body>
+
+</html>
