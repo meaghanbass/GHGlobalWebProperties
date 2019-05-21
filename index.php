cea660125306534447e67e04442d517c892be4e9
Meaghan Bass <45584434+meaghanbass@users.noreply.github.com>
2019-05-21 14:02:04 -0700

    fixed footer

--- index.php
+++ index.php
@@ -15,15 +15,13 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#000000">
 
   <link rel="manifest" href="./manifest.webmanifest">
-  <link rel="shortcut icon" href="images/favicon.png">
+  <link rel="shortcut icon" href="images/favicon_blue.png">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css">
   <link rel="stylesheet" href="./src/index.css">
   <link rel="stylesheet" href="./src/app.css">
-
 
   <link rel="stylesheet" href="css/rootcolors.css" type="text/css">
   <link rel="stylesheet" href="css/styles.css" type="text/css">
@@ -39,7 +37,6 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   
-
   <title>Global Web Properties</title>
 </head>
 
--- php/footer.php
+++ php/footer.php
@@ -1,66 +1,61 @@
 <footer class="row telluric">
   <div class="col-md-offset-2 col-md-3 col-sm">
       <ul class="footer_ul font-hind">
-        <h4 class="font-poppins solstice">Properties</h4>
-        <li>Stockpress.shop</li>
-        <li>Techbuilds.co</li>
-        <li>Youtheducationsociety.org</li>
-        <li>Thefinancialaffairs.com</li>
-        <li>Apexconsultgroup.com</li>
-        <li>Reprosites.co</li>
-        <li>Realestatedesign.co</li>
-        <li class="font-poppins solstice"><strong>SEARCH ALL PROPERTIES</strong></li>
+        <h4 class="font-poppins solstice">Partners</h4>
+        <li>Bluehost</li>
+        <li>Amazon Web Services (AWS)</li>
+        <li>Google Cloud Platform</li>
+        <li>Digital Ocean</li>
+        <li>VMWare</li>
+        <li>GoDaddy</li>
+        <li>Salesforce</li>
       </ul>
   </div>
   <div class="col-md-3 col-sm">
       <ul class="footer_ul font-hind">
-        <h4 class="font-poppins solstice">Availabilities</h4>
-        <li>Domains</li>
-        <li>Companies</li>
-          <br>
-          <h4 class="font-poppins solstice">Resources</h4>
-        <li>Reports</li>
-        <li>Forms</li>
-        <li>Request For Information (RFI)</li>
+        <h4 class="font-poppins solstice">Services</h4>
+        <li>Web Design & Development</li>
+        <li>Managed Services (Hosting & Site Maintenance)</li>
+        <li>App Development</li>
+        <li>Solutions Architecture</li>
+        <br>
+        <h4 class="font-poppins solstice">Resources</h4>
+        <li>Request For Proposal (RFP)</li>
       </ul>
-  </div>
-  <div class="col-md-3 col-sm">
+      <br>
       <ul class="footer_ul font-hind">
         <h4 class="contact font-poppins nebula pointer">Contact Us</h4>
       </ul>
+  </div>
+  <div class="col-md-3 col-sm">
+      
+      <!-- Begin Mailchimp Signup Form -->
+      <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+      <div id="mc_embed_signup">
+      <form action="https://gmail.us20.list-manage.com/subscribe/post?u=6324e563bdd15c95ab519a5bc&amp;id=973c822e15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll" style="padding-top:0 !important;">
+        <h2>Subscribe For Updates</h2>
 
+      <div class="mc-field-group">
+        <label for="mce-FNAME" class="form-label">Name </label>
+        <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+      </div>
 
-<!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<div id="mc_embed_signup">
-<form action="https://gmail.us20.list-manage.com/subscribe/post?u=6324e563bdd15c95ab519a5bc&amp;id=973c822e15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll" style="padding-top:0 !important;">
-	<h2>Subscribe For Updates</h2>
+      <div class="mc-field-group">
+        <label for="mce-EMAIL" class="form-label">Email </label>
+        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+      </div>
 
-<div class="mc-field-group">
-	<label for="mce-FNAME" class="form-label">Name </label>
-	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-</div>
-
-<div class="mc-field-group">
-	<label for="mce-EMAIL" class="form-label">Email </label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6324e563bdd15c95ab519a5bc_973c822e15" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button subscribe-button"></div>
-    </div>
-</form>
-</div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[5]='INQUIRY';ftypes[5]='dropdown';fnames[2]='COMPANY';ftypes[2]='text';fnames[1]='FNAME';ftypes[1]='text';fnames[3]='LNAME';ftypes[3]='text';fnames[4]='PHONE';ftypes[4]='phone';fnames[0]='EMAIL';ftypes[0]='email';fnames[6]='MESSAGE';ftypes[6]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-<!--End mc_embed_signup-->
-
-
-
-
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6324e563bdd15c95ab519a5bc_973c822e15" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button subscribe-button"></div>
+          </div>
+      </form>
+      </div>
+      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[5]='INQUIRY';ftypes[5]='dropdown';fnames[2]='COMPANY';ftypes[2]='text';fnames[1]='FNAME';ftypes[1]='text';fnames[3]='LNAME';ftypes[3]='text';fnames[4]='PHONE';ftypes[4]='phone';fnames[0]='EMAIL';ftypes[0]='email';fnames[6]='MESSAGE';ftypes[6]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+      <!--End mc_embed_signup-->
   </div>
 </footer>
